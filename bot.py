ee44045d5a2580eaecbb97637e3502eb5721f883
Roman <rocar1991@gmail.com>
2016-11-18 11:09:38 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
+import config
 import telebot
 
-bot = telebot.TeleBot("284386746:AAHpCwRTpJufHyb54Z-ODYjqjRv29YH_30k")
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
